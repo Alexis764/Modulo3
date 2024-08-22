58606aa87c818c61483bb0ee979994c7f9605568
AlexisSenaWS <arevaloalexis764@gmail.com>
2024-08-22 11:20:07 -0500

    RegisterScreen

--- app/src/main/java/com/example/projectsourcetree/MainActivity.kt
+++ app/src/main/java/com/example/projectsourcetree/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.projectsourcetree.ui.theme.ProjectSourceTreeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +27,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProjectSourceTreeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    RegisterScreen(Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ProjectSourceTreeTheme {
-        Greeting("Android")
+fun RegisterScreen(modifier: Modifier = Modifier) {
+    var emailValue by rememberSaveable { mutableStateOf("") }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 24.dp)
+    ) {
+        OutlinedTextField(
+            value = emailValue,
+            onValueChange = { emailValue = it },
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
